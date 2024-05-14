77f59bd283f77dfdc33dfbe4dce1a3c9f28bcc36
Nobuta Taiki <66196738+taikiwish@users.noreply.github.com>
2024-05-14 19:09:06 +0900

    userモデル単体テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname { Faker::Name.initials(number: 2) }
-    email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 6) }
-    password_confirmation { |u| u.password }
-    familyname { Faker::Name.last_name }
-    firstname { Faker::Name.first_name }
-    familyname_kana { Faker::Japanese::Name.last_name }
-    firstname_kana { Faker::Japanese::Name.first_name }
-    birthday { Faker::Date.birthday(min_age: 18, max_age: 65) }
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    familyname            { Faker::Name.last_name }
+    firstname             { Faker::Name.first_name }
+    familyname_kana       { Faker::Name.last_name }
+    firstname_kana        { Faker::Name.first_name }
+    birthday              { Faker::Date.between(from: '1930-01-01', to: '2023-12-31') }
   end
 end
