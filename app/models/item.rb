d9ed379944d3db4c92e6b43b7b0ce0831ff0421e
Nobuta Taiki <66196738+taikiwish@users.noreply.github.com>
2024-05-15 16:22:36 +0900

    ActiveHashのアソシエーション、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,12 @@
 class Item < ApplicationRecord
   belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :fee
+  belongs_to :area
+  belongs_to :date
   has_one_attached :image
 
   validates :name,:description,:category_id,:condition_id,:fee_id,:area_id,:date_id,:price,:user, presence: true
+  validates :category_id, :condition_id, :fee_id, :area_id, :date_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
