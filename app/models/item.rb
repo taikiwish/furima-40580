41de3d08b09e5dfaf98840ecba72f13912bb1375
Nobuta Taiki <66196738+taikiwish@users.noreply.github.com>
2024-05-15 17:59:47 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,5 @@ class Item < ApplicationRecord
 
   validates :name,:description,:category_id,:condition_id,:fee_id,:area_id,:date_id,:price,:user, presence: true
   validates :category_id, :condition_id, :fee_id, :area_id, :date_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :price, format: { with: /\A[3-9]\d{2,6}\z/, message: "¥300以上¥9,999,999以下の半角数値で入力してください" }
 end
