cfbd61d2fda36d5ca63b6e1534c5277b6efffd92
Nobuta Taiki <66196738+taikiwish@users.noreply.github.com>
2024-05-17 16:07:44 +0900

    テストコードの追記、コメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
-    @items = Item.all
+    # @items = Item.all
   end
 
   def new
@@ -9,7 +9,7 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
     if @item.save
       redirect_to '/'
     else
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -32,8 +32,18 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Category can't be blank"
       end
+      it 'category_idに「---」が選択されている場合は登録できない' do
+        @item.category_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category can't be blank"
+      end
       it 'condition_idが空では登録できない' do
         @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Condition can't be blank"
+      end
+      it 'condition_idに「---」が選択されている場合は登録できない' do
+        @item.condition_id = '---'
         @item.valid?
         expect(@item.errors.full_messages).to include "Condition can't be blank"
       end
@@ -42,13 +52,28 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Fee can't be blank"
       end
+      it 'fee_idに「---」が選択されている場合は登録できない' do
+        @item.fee_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Fee can't be blank"
+      end
       it 'area_idが空では登録できない' do
         @item.area_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include "Area can't be blank"
       end
+      it 'area_idに「---」が選択されている場合は登録できない' do
+        @item.area_id = '---'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Area can't be blank"
+      end
       it 'deliverydate_idが空では登録できない' do
         @item.deliverydate_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Deliverydate can't be blank"
+      end
+      it 'deliverydate_idに「---」が選択されている場合は登録できない' do
+        @item.deliverydate_id = '---'
         @item.valid?
         expect(@item.errors.full_messages).to include "Deliverydate can't be blank"
       end
@@ -76,6 +101,11 @@ RSpec.describe Item, type: :model do
         @item.price = 'あああ'
         @item.valid?
         expect(@item.errors.full_messages).to include "Price ¥300以上¥9,999,999以下の半角数値で入力してください"
+      end
+      it 'userが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+         expect(@item.errors.full_messages).to include "User must exist"
       end
     end
   end
