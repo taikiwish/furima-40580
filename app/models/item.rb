6c6fe6e17de6702d050f7e97f90df199a0fc9b18
Nobuta Taiki <66196738+taikiwish@users.noreply.github.com>
2024-05-17 16:31:32 +0900

    テストコード修正2

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,5 @@ class Item < ApplicationRecord
 
   validates :image,:name,:description,:category_id,:condition_id,:fee_id,:area_id,:deliverydate_id,:price,:user, presence: true
   validates :category_id, :condition_id, :fee_id, :area_id, :deliverydate_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-  validates :price, format: { with: /\A[3-9]\d{2,6}\z/, message: "¥300以上¥9,999,999以下の半角数値で入力してください" }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "は300以上9,999,999以下の半角数値で入力してください"}
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Category can't be blank"
       end
       it 'category_idに「---」が選択されている場合は登録できない' do
-        @item.category_id = '---'
+        @item.category_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include "Category can't be blank"
       end
@@ -43,7 +43,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Condition can't be blank"
       end
       it 'condition_idに「---」が選択されている場合は登録できない' do
-        @item.condition_id = '---'
+        @item.condition_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include "Condition can't be blank"
       end
@@ -53,7 +53,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Fee can't be blank"
       end
       it 'fee_idに「---」が選択されている場合は登録できない' do
-        @item.fee_id = '---'
+        @item.fee_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include "Fee can't be blank"
       end
@@ -63,7 +63,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Area can't be blank"
       end
       it 'area_idに「---」が選択されている場合は登録できない' do
-        @item.area_id = '---'
+        @item.area_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include "Area can't be blank"
       end
@@ -73,7 +73,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include "Deliverydate can't be blank"
       end
       it 'deliverydate_idに「---」が選択されている場合は登録できない' do
-        @item.deliverydate_id = '---'
+        @item.deliverydate_id = '1'
         @item.valid?
         expect(@item.errors.full_messages).to include "Deliverydate can't be blank"
       end
@@ -85,22 +85,22 @@ RSpec.describe Item, type: :model do
       it 'priceが¥299以下では登録できない' do
         @item.price = '299'
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price ¥300以上¥9,999,999以下の半角数値で入力してください"
+        expect(@item.errors.full_messages).to include "Price は300以上9,999,999以下の半角数値で入力してください"
       end
       it 'priceが¥10,000,000以下では登録できない' do
         @item.price = '10,000,001'
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price ¥300以上¥9,999,999以下の半角数値で入力してください"
+        expect(@item.errors.full_messages).to include "Price は300以上9,999,999以下の半角数値で入力してください"
       end
       it 'priceが半角英字では登録できない' do
         @item.price = 'aaa'
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price ¥300以上¥9,999,999以下の半角数値で入力してください"
+        expect(@item.errors.full_messages).to include "Price は300以上9,999,999以下の半角数値で入力してください"
       end
       it 'priceが全角では登録できない' do
         @item.price = 'あああ'
         @item.valid?
-        expect(@item.errors.full_messages).to include "Price ¥300以上¥9,999,999以下の半角数値で入力してください"
+        expect(@item.errors.full_messages).to include "Price は300以上9,999,999以下の半角数値で入力してください"
       end
       it 'userが紐付いていなければ出品できない' do
         @item.user = nil
