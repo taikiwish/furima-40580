ac5bf85a9f3232539c9e71c477b3a715b17e1397
Nobuta Taiki <66196738+taikiwish@users.noreply.github.com>
2024-05-17 15:38:07 +0900

    imageバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   belongs_to :deliverydate
   has_one_attached :image
 
-  validates :name,:description,:category_id,:condition_id,:fee_id,:area_id,:deliverydate_id,:price,:user, presence: true
+  validates :image,:name,:description,:category_id,:condition_id,:fee_id,:area_id,:deliverydate_id,:price,:user, presence: true
   validates :category_id, :condition_id, :fee_id, :area_id, :deliverydate_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :price, format: { with: /\A[3-9]\d{2,6}\z/, message: "¥300以上¥9,999,999以下の半角数値で入力してください" }
 end
