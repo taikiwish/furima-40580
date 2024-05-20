15df787c898ed9e093acdbedb199c6d6030fbfda
Nobuta Taiki <66196738+taikiwish@users.noreply.github.com>
2024-05-20 15:32:26 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -17,9 +17,9 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def show
-  #   @item = Tweet.find(params[:id])
-  # end
+  def show
+    @item = Item.find(params[:id])
+  end
 
   private
   def item_params
