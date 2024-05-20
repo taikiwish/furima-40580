5b5cd71f37e2880b54d557e0e1acd318df0bac5d
Nobuta Taiki <66196738+taikiwish@users.noreply.github.com>
2024-05-20 20:18:09 +0900

    destroyアクションに条件分岐追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,9 +37,13 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    @item = Item.find(params[:id])
+    if current_user && @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
